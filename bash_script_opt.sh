068f054fd447965d6f550d7c0c18c346b5b34bf6
Mehdi Esmaeili Fard <mehdi.esmaeilifard@gmail.com>
2021-01-23 02:55:35 +0330

    Update bash_script_opt.sh
    Edit

--- bash_script_opt.sh
+++ bash_script_opt.sh
@@ -2,22 +2,22 @@
 START=`date +%s`
 echo ""
 
-# Var define
+# Making directories 
+mkdir -p intermediate_sh
+mkdir -p log_sh
+mkdir -p results_sh
+
+# Defining variables
 INPUT="data/info_files/metadata.csv"
 VCF_DIR="data/vcfs/"
 DIR="intermediate_sh"
 LOG="log_sh"
 RES="results_sh"
-mkdir -p intermediate_sh
-mkdir -p log_sh
-mkdir -p results_sh
 
-#Parsing Info file
+# Parsing Info file
 echo "# Parsing Info file ..."
 cut -d"," -f1 $INPUT | sed '1d' | while read line; do echo $VCF_DIR${line}; done > $DIR/vcfnames.list # VCFs names
 cut -d"," -f1 --complement $INPUT | awk 'NR==1 || NR==2' > $DIR/qcthr.csv # Thresholds
-
-# cut -d"," -f1 $INPUT | sed '1d' | ts $VCF_DIR | tr -d ' ' > $DIR/vcfnames.list # moreutils pkg needed for this line (sudo apt install moreutils).
 
 # A while loop to assign each threshold value to its name (assigns value of each field to field name)
 nr=1
@@ -34,7 +34,7 @@ done < $DIR/qcthr.csv
 
 # Merging VCF files
 echo "# Merging VCF files ..."
-PicardCommandLine MergeVcfs I=$DIR/vcfnames.list O=$DIR/merged.vcf.gz 2> $LOG/merge.log  #/dev/null # Merging VCF files using Picard
+PicardCommandLine MergeVcfs I=$DIR/vcfnames.list O=$DIR/merged.vcf.gz 2> $LOG/merge.log
 # rm $DIR/vcfnames.list $DIR/qcthr.csv
 
 # Quality control
@@ -44,9 +44,9 @@ echo "Minor Allel Frequency: $MAF"
 echo "Hardy-Weinberg test: $HWE"
 echo "Missing genotype per site: $Missing_per_Mrks"
 echo "Missing genotype per individual: $Missing_per_Inds"
-vcftools --gzvcf $DIR/merged.vcf.gz --missing-indv --out $DIR/miss_data_per_inds 2> $LOG/miss_per_inds.log  #/dev/null # Missingness on a per-individual basis.
-awk '$5 > $Missing_per_Inds {print $1}' $DIR/miss_data_per_inds.imiss | sed '1d' > $DIR/high_miss_inds # Making a list of Inds with missing genetypes rate higher than defined thrshold.
-vcftools --gzvcf $DIR/merged.vcf.gz --remove $DIR/high_miss_inds --max-missing $Missing_per_Mrks --maf $MAF --hwe $HWE --recode --recode-INFO-all --out $DIR/merged_qcok.vcf --stdout | bgzip -c > $DIR/merged_qcok.vcf.gz 2> $LOG/qc.log # QC
+vcftools --gzvcf $DIR/merged.vcf.gz --missing-indv --out $DIR/miss_data_per_inds 2> $LOG/miss_per_inds.log
+awk '$5 > $Missing_per_Inds {print $1}' $DIR/miss_data_per_inds.imiss | sed '1d' > $DIR/high_miss_inds
+vcftools --gzvcf $DIR/merged.vcf.gz --remove $DIR/high_miss_inds --max-missing $Missing_per_Mrks --maf $MAF --hwe $HWE --recode --recode-INFO-all --out $DIR/merged_qcok.vcf --stdout | bgzip -c > $DIR/merged_qcok.vcf.gz 2> $LOG/qc.log
 sed -n 19p $DIR/merged_qcok.vcf.log; sed -n 21p $DIR/merged_qcok.vcf.log
 # rm $DIR/merged.vcf.gz $DIR/merged.vcf.gz.tbi
 
@@ -56,10 +56,10 @@ plink2 --vcf $DIR/merged_qcok.vcf.gz --king-cutoff 0.0442 --out $DIR/filter  &> 
 grep -v "IID" $DIR/filter.king.cutoff.in.id > $RES/list_of_unrelated_individuals.txt
 echo "List of unrelated individuals created in $RES directory."
 echo "# Check remained individuals for kinship ..."
-plink2 --vcf $DIR/merged_qcok.vcf.gz --remove $DIR/filter.king.cutoff.out.id --make-king-table --out $DIR/merged_qc_unrel_kin &> $LOG/plink_kin.table.log  #/dev/null # Making kinship table with unrelated Individuals
+plink2 --vcf $DIR/merged_qcok.vcf.gz --remove $DIR/filter.king.cutoff.out.id --make-king-table --out $DIR/merged_qc_unrel_kin &> $LOG/plink_kin.table.log
 Rscript "src/kinship_check _opt_bash.R"
 echo "# Removing related Individulas from merged VCF file"
-bcftools view -S "$RES/list_of_unrelated_individuals.txt" "$DIR/merged_qcok.vcf.gz" | bgzip -c > $DIR/merged_clean.vcf.gz 2> $LOG/filter_related.log
+bcftools view -S "$RES/list_of_unrelated_individuals.txt" "$DIR/merged_qcok.vcf.gz" | bcftools annotate -x INFO | bgzip -c > $DIR/merged_clean.vcf.gz 2> $LOG/filter_related.log
 # rm $DIR/merged_qcok.vcf.gz
 
 # Annotating merged vcf file
